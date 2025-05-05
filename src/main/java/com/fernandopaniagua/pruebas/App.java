af12a4b3dbbd123d1779b06e5521be5cee37b9f8
fpaniaguajava <fernando.paniagua.java@gmail.com>
2025-05-05 20:58:11 +0200

    Initial commit

---
+++ src/main/java/com/fernandopaniagua/pruebas/App.java
@@ -0,0 +1,13 @@
+package com.fernandopaniagua.pruebas;
+
+import com.fernandopaniagua.pruebas.util.Calculadora;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        int resultado = Calculadora.sumar(5,8);
+        System.out.print("Resultado:" + resultado);
+    }
+}
