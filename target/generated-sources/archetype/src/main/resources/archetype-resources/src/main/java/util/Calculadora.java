b3577a198512ae6f53f91ee742b98dd1b1fa0daf
fpaniaguajava <fernando.paniagua.java@gmail.com>
2025-05-06 19:20:36 +0200

    Pruebas

---
+++ target/generated-sources/archetype/src/main/resources/archetype-resources/src/main/java/util/Calculadora.java
@@ -0,0 +1,19 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.util;
+
+public class Calculadora {
+    public static int sumar(int s1, int s2) {
+        return s1 + s2;
+    }
+    public static int restar(int r1, int r2) {
+        return r1 - r2;
+    }
+    public static int multiplicar(int m1, int m2) {
+        return m1 * m2;
+    }
+    public static int dividir(int d1, int d2) {
+        return d1 / d2;
+    }
+}
